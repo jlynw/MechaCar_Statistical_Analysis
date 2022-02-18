57aa1e2ca5477883aa6ae0cae9d2e02355347047
JudyW <92497511+jlynw@users.noreply.github.com>
2022-02-18 11:02:15 -0800

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -9,3 +9,18 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 #p-value & r-square value
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mechacar))
+
+#import & read
+coil <- read.csv('Suspension_Coil.csv',stringsAsFactors = F)
+
+#total_summary
+total_summary <- coil %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+
+#lot_summary
+lot_summary <- coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+
+#t-test
+t.test(coil$PSI,mu=1500)
+t.test(subset(coil$PSI,coil$Manufacturing_Lot=='Lot1'),mu=1500)
+t.test(subset(coil$PSI,coil$Manufacturing_Lot=='Lot2'),mu=1500)
+t.test(subset(coil$PSI,coil$Manufacturing_Lot=='Lot3'),mu=1500)
